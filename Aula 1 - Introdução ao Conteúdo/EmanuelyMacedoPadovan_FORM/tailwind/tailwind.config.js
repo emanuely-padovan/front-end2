ff6a4b6970716a6ab5a7c2cdfcf5410c3ce6a0e9
emanuely-padovan <emanuely.padovan@aluno.senai.br>
2026-08-14 22:54:02 -0300

    "Finalização" da "Parte 2 - Tailwind"

---
+++ Aula 1 - Introdução ao Conteúdo/EmanuelyMacedoPadovan_FORM/tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./*.js",
+  ],
+  theme: {
+    extend: {
+      // Configurações personalizadas...
+    },
+  },
+  plugins: [],
+}
